1eb81d99d7dcb44e6ed97beccf5ea8c033a65c7c
Grace Chen <grace.s.chen@duke.edu>
2015-02-25 16:48:17 -0500

    test

--- UDP_Socket.c
+++ UDP_Socket.c
@@ -6,14 +6,14 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int server(Str address, uint16_t port)
-{
+int server(Str address, uint16_t port) {
+
 	int sock, accepted_client, client_addr_len;
 	struct sockaddr_in socket_addr, client_addr;
 	pid_t pid;
 	char packet[//some lwngth]
 
-  /* Create UDP socket*/
+  	/* Create UDP socket*/
 	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 		perror("UDP socket creation error:");
 		return 1;
@@ -37,20 +37,20 @@ int server(Str address, uint16_t port)
 	listen(sock, 0);
 	
 	/* Accept only one connection*/	
-  accepted_client = accept4(sock, (struct sockaddr *) &client_addr, &client_addr_len,0);
+  	accepted_client = accept4(sock, (struct sockaddr *) &client_addr, &client_addr_len,0);
 	printf("Accepted client: %i \n", accepted_client);
 	if (accepted_client < 0) {
       		perror("Accept error");
       		return 1;
-  }
-	while(1){ 
+  	}
+	while(1) { 
 	
 		if(recv(accepted_client, packet, //some packet length, 0) < 0) {
 			perror("Recv error:");
 			return 1;
 		}
 
-		memset(reply,0,sizeof(reply));
+		memset(reply, 0, sizeof(reply));
 		reply[strlen(reply)] = '\0';
 		if (send(//something) < 0) {
 			perror("Send error:");
