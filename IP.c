e2b55b2f27a19424b22ede205758c5e646c2ca4f
Tianchan Dong <td84@duke.edu>
2015-02-28 02:13:38 -0500

    Update IP.c
    Main IP functionality added

--- IP.c
+++ IP.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
-#include "ipsum.c"
-#include "IPRIPInterface.c"
-#include "IPUDPInterface.c"
-#include <ip.h>
+#include <stdlib.h>
+#include </usr/include/netinet/ip.h>
+#include "ipsum.h"
 
-//recalculate checksum
-//decrement TTL
+struct IPpacket{
+	struct ip header;
+	char payload[IP_MAXPACKET-sizeof(struct ip)];
+}
 
-struct iphdr *ip, *ipreply;
-int TTL;
-int checksum;
 
-int main() {
-	checksum = ip_sum(message, random integer!?);
+int is_RIP(struct ip *{header){
+	//check whether to deliver to RIP
+	if(header->ip_p == 200){ //RIP
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+int process_header_for_forwarding(struct ip *header){
+	//Decrement TTL
+	header->ip_ttl = (u_char) ((int)header->ip_ttl-1);
+	//Recomputer Checksum
+	header->ip_sum = ip_sum(header, sizeof(struct ip));
+	return 0;
 }
