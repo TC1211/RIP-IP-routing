5a4ab5b9137c2167d60531f8d0ddd6b171f3a2c1
Tianchan Dong <td84@duke.edu>
2015-03-01 03:18:56 -0500

    Establish proper interface

--- UDPIPInterface.c
+++ UDPIPInterface.c
@@ -1,6 +1,13 @@
 #include "IP.c"
 #include "UDPSocket.h"
 
+int set_up_recv_sock(int *sock,char *addr, uint16_t port, char *received_packet){
+	struct sockaddr_in receivedfrom_addr;
+	recv_sockaddr(sock,addr, port, &receivedfrom_addr);
+	sock_recv(sock, &receivedfrom_addr, received_packet);
+	return 0;
+}
+
 int UDPtoIP(char *packet){
 
 }
